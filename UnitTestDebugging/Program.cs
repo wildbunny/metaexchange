f337cc66020fc5c32a43d82e6b36297bb09d66b7
Paul Firth <paul@wildbunny.co.uk>
2015-01-24 11:27:35 +0000

    more missing dependencies

---
+++ UnitTestDebugging/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BitsharesCoreUnitTests;
+
+namespace UnitTestDebugging
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			AddressAndPubKeyTests test = new AddressAndPubKeyTests();
+
+			test.CheckBtsAddressFromBitcoinPubKeyHex();
+		}
+	}
+}
